17ef0804d885aacdbead3846399bb65e735c5718
Mahesh <mahesh51138@gmail.com>
2022-01-28 09:07:06 +0530

    added:game

---
+++ src/com/bridgelabz/TIcTacToeGame/TicTacToeGame.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.TIcTacToeGame;
+
+public class TicTacToeGame {
+
+    static void read() {
+
+        int ar[] = new int[10];
+        for (int i = 1; i < ar.length; i++) {
+            ar[i] = ' ';
+        }
+        System.out.println("Task Completed");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Task Started");
+        read();
+    }
+}
