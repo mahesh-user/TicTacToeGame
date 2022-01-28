85194d0edfd0b6dc3d5ec071a17bcc1ed8db0f39
Mahesh <mahesh51138@gmail.com>
2022-01-28 09:38:24 +0530

    added:PlayerEntry

--- src/com/bridgelabz/TIcTacToeGame/TicTacToeGame.java
+++ src/com/bridgelabz/TIcTacToeGame/TicTacToeGame.java
@@ -1,18 +1,35 @@
 package com.bridgelabz.TIcTacToeGame;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
-    static void read() {
-
-        int ar[] = new int[10];
+    static char ar[] = new char[10];
+    static void create() {
         for (int i = 1; i < ar.length; i++) {
             ar[i] = ' ';
         }
         System.out.println("Task Completed");
     }
 
+    static void read(){
+        for(int i=1;i<ar.length;i++) {
+            System.out.println("Enter 0 for user entry and 1 for computer");
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt();
+            if (a == 0) {
+                System.out.println("Enter 0 or X");
+                char s = scan.next().charAt(0);
+                ar[i] = s;
+                System.out.println("entered input is : "+ar[i]);
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Task Started");
+        create();
         read();
     }
 }
